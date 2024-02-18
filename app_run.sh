8266813151bb38d02d0133b0a6132b53eebda8fe
Marc <manjaroman2@protonmail.com>
2024-02-18 13:30:17 +0100

    asd

--- app_run.sh
+++
@@ -1,1 +0,0 @@
-python app_kivy/main.py
--- compile_bootloader.sh
+++
@@ -1,3 +0,0 @@
-# wine C:/Python38/python.exe bildverkleinerer.py 
-wine C:/Python38/Scripts/pyinstaller.exe --noupx -F bootloader.py
-pyinstaller --noupx -F bootloader.py
